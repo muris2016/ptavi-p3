9ddb803c4f06b9b1ef2c3ec29d88289fdbe561c3
muris2016 <rsemuris2016@gmail.com>
2015-10-12 17:01:22 +0200

    Nombres de variables mejorados

--- karaoke.py
+++ karaoke.py
@@ -7,45 +7,45 @@ from smallsmilhandler import SmallSMILHandler
 import json
 import urllib.request
 
+
 def take_filename():
     if len(sys.argv) != 2:
         sys.exit("Usage:  python3 karaoke.py file.smil")
     else:
         return sys.argv[1]
 
-class KaraokeLocal(SmallSMILHandler):
-    def __init__ (self, filename):
+
+class KaraokeLocal():
+    def __init__(self, filename):
         parser = make_parser()
         cHandler = SmallSMILHandler()
         parser.setContentHandler(cHandler)
         parser.parse(open(filename))
-        self.list_labels = cHandler.get_tags()
+        self.list_tags = cHandler.get_tags()
 
-
-    def __str__ (self):
-        str_label = ''
-        for label in self.list_labels:
-            str_label += label[0]
-            for key, value in label[1].items():
+    def __str__(self):
+        str_tags = ''
+        for tag in self.list_tags:
+            str_tags += tag[0] # tag[0] is the tag name
+            for key, value in tag[1].items(): # tag[1] is the attributes dict
                 if value != '':
-                    str_label += '\\%s="%s"\\n' % (key, value)
-                    # str_label += '\\' + key + '=' + '"'+ value + '"' + '\\n'
-            if label != self.list_labels[-1]:
-                str_label += '\n'
-        return str_label
+                    str_tags += '\t%s="%s"' % (key, value)
+            if tag != self.list_tags[-1]:
+                str_tags += '\n'
+        return str_tags
 
-    def to_json (self, smile_file, json_file = ''):
+    def to_json(self, smile_file, json_file=''):
         if json_file == '':
             json_file = smile_file.split('.')[0] + '.json'
-        data = json.dumps(self.list_labels)
+        data = json.dumps(self.list_tags)
         with open(json_file, 'w') as outfile:
             json.dump(data, outfile)
 
-    def do_local (self):
-        for label in self.list_labels:
-            attributes = label[1]
+    def do_local(self):
+        for tag in self.list_tags:
+            attributes = tag[1]
             for key in attributes:
-                if 'http://' in attributes[key]:
+                if 'src' in attributes and 'http://' in attributes[key]:
                     filename = attributes[key].split('/')[-1]
                     urllib.request.urlretrieve(attributes[key], filename)
                     attributes[key] = filename
@@ -53,7 +53,7 @@ class KaraokeLocal(SmallSMILHandler):
 if __name__ == "__main__":
     filename = take_filename()
     cHandler = KaraokeLocal(filename)
-    print(cHandler.__str__())
+    print('\n' + cHandler.__str__() + '\n')
     cHandler.to_json(filename)
     cHandler.do_local()
-    print(cHandler.__str__())
+    print('\n' + cHandler.__str__() + '\n')
