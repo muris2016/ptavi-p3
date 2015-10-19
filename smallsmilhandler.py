01388d4d3e399b5e11b202f00cf716933f28d7a7
muris2016 <rsemuris2016@gmail.com>
2015-10-19 16:07:42 +0200

    Dict comprehension implementado

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
 class SmallSMILHandler(ContentHandler):
     def __init__ (self):
-        self.list_labels = []
         root_layout = {'width': '', 'height': '', 'background-color': ''}
         region = {'id': '', 'top': '', 'bottom': '', 'left': '', 'right': ''}
         img = {'src': '', 'region': '', 'begin': '', 'dur': ''}
@@ -15,22 +13,11 @@ class SmallSMILHandler(ContentHandler):
         self.labels = {'root-layout': root_layout, 'region': region,'img': img,
                        'audio': audio, 'textstream': textstream}
 
-
+        self.list_labels = []
     def startElement(self, name, attrs):
         if name in self.labels:
-            # if name == 'region':
-                my_dict = {}
-                for key in self.labels[name].keys():
-                    # if 'src'in self.labels[name]:
-                        my_dict[key] = attrs.get(key, "")
-                self.list_labels.append([name, my_dict])
+            my_dict = {key: attrs.get(key, '') for key in self.labels[name]}
+            self.list_labels.append([name, my_dict])
 
     def get_tags(self):
         return self.list_labels
-
-if __name__ == '__main__':
-    filename = sys.argv[1]
-    parser = make_parser()
-    cHandler = SmallSMILHandler()
-    parser.setContentHandler(cHandler)
-    parser.parse(open(filename))
