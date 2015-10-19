7c40cc9ec253b2f89b272cb211d7bd50d218211f
muris2016 <rsemuris2016@gmail.com>
2015-10-19 16:10:42 +0200

    pep8 pasado

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -3,17 +3,18 @@
 
 from xml.sax.handler import ContentHandler
 
+
 class SmallSMILHandler(ContentHandler):
-    def __init__ (self):
+    def __init__(self):
         root_layout = {'width': '', 'height': '', 'background-color': ''}
         region = {'id': '', 'top': '', 'bottom': '', 'left': '', 'right': ''}
         img = {'src': '', 'region': '', 'begin': '', 'dur': ''}
         audio = {'src': '', 'begin': '', 'dur': ''}
         textstream = {'src': '', 'region': ''}
-        self.labels = {'root-layout': root_layout, 'region': region,'img': img,
-                       'audio': audio, 'textstream': textstream}
-
+        self.labels = {'root-layout': root_layout, 'region': region,
+                       'img': img, 'audio': audio, 'textstream': textstream}
         self.list_labels = []
+
     def startElement(self, name, attrs):
         if name in self.labels:
             my_dict = {key: attrs.get(key, '') for key in self.labels[name]}
